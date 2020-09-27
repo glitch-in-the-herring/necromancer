50dd485460af4da81511b9f5a2370905554ca181
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2020-09-27 07:24:54 +0700

    oop be my friend pls

--- utils/updater.py
+++ utils/updater.py
@@ -4,11 +4,12 @@ from discord.ext import commands
 class Updater(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
+		self.database = self.bot.get_cog("Database")
 
 	@commands.command()
-	async def test(self, ctx):
-		database = self.bot.get_cog("Database")
-		database.add_server("this", "works")
+	async def add_channel(self, ctx, channel: discord.TextChannel):
+		self.database.add_server(ctx.guild.id, channel.id)
+		await ctx.send(f"Successfully set {channel} as a necromancy channel for this server.")
 
 def setup(bot):
     bot.add_cog(Updater(bot))
