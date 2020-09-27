f55ae5767138b68453eec0a69d0e864b1aa09bbd
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2020-09-27 10:34:11 +0700

    note to self: i suck at oop

--- utils/converter.py
+++ utils/converter.py
@@ -6,12 +6,12 @@ class Converter(commands.Cog):
 		self.bot = bot
 
 	# Converts a timedelta object into seconds
-	def delta_to_secs(delta):
+	def delta_to_secs(self, delta):
 		return delta.days * 86400 + delta.seconds
 
 
 	# Converts a number of seconds into a tuple consisting of hours, minutes, and seconds
-	def secs_to_hms(seconds):
+	def secs_to_hms(self, seconds):
 		hours, remainder = divmod(seconds, 3600)
 		minutes, seconds = divmod(remainder, 60)
 		return hours, minutes, seconds
