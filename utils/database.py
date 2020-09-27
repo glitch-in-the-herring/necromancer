00f60416c40d37c4c0c584f746dae6ee229d7ddf
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2020-09-27 07:41:12 +0700

    i don't know if this will work

--- utils/database.py
+++ utils/database.py
@@ -1,8 +1,16 @@
 import sqlite3
+from discord.ext import commands
 
 conn = sqlite3.connect("test.db")
 c = conn.cursor()
 
-def add_server(guild_id, channel_id):
-	c.execute("INSERT OR REPLACE INTO test (guild_id, channel_id) VALUES (?, ?)", [guild_id, channel_id])
-	conn.commit()
+class Database(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+	def add_server(self, guild_id, channel_id):
+		c.execute("INSERT OR REPLACE INTO test (guild_id, channel_id) VALUES (?, ?)", [guild_id, channel_id])
+		conn.commit()
+
+def setup(bot):
+    bot.add_cog(Database(bot))
--- utils/updater.py
+++ utils/updater.py
@@ -7,10 +7,11 @@ importlib.import_module("database")
 class Updater(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
+		self.database = bot.get_cog("Database")
 
 	@commands.command()
 	async def add_channel(self, ctx, channel: discord.TextChannel):
-		database.add_server(ctx.guild.id, channel.id)
+		self.database.add_server(ctx.guild.id, channel.id)
 		await ctx.send(f"Successfully set {channel} as a necromancy channel for this server.")
 
 def setup(bot):
