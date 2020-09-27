8ec417ad86b428fda5d8ab285fb17773c763d3df
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2020-09-27 07:08:11 +0700

    i hope this works:

--- utils/updater.py
+++ utils/updater.py
@@ -5,10 +5,9 @@ class Updater(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 
-	database = self.bot.get_cog("Database")
-
 	@commands.command
 	async def test(self, ctx):
+		database = self.bot.get_cog("Database")
 		database.add_server("this", "works")
 
 def setup(bot):
