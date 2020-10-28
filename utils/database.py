eb7b183af0251b4a18746cbb5e966f53efef09bc
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2020-10-28 20:32:15 +0700

    duh

--- utils/database.py
+++ utils/database.py
@@ -15,7 +15,7 @@ class Database(commands.Cog):
 
 	# Adds or updates channel_id and guild_id in the guilds table
 	def update_server(self, guild_id, channel_id):
-		c.execute("INSERT OR REPLACE INTO guilds (guild_id, channel_id, last_message_id) VALUES (?, ?, ?)", [guild_id, channel_id, last_message_id])
+		c.execute("INSERT OR REPLACE INTO guilds (guild_id, channel_id) VALUES (?, ?)", [guild_id, channel_id])
 
 
 	# Updates the last message in a game channel
