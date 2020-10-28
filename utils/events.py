c50dac8899b91457b8d4424f33cc04769472208a
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2020-10-28 20:26:43 +0700

    probably a good idea to have this

---
+++ utils/events.py
@@ -0,0 +1,16 @@
+from discord.ext import commands
+
+class Events(commands.Cog):
+	def __init__(self, bot):
+		self.bot = bot
+
+
+	# Listeners
+	@commands.Cog.listener()
+	async def on_ready(self):
+		print("We have logged in")
+
+
+def setup(bot):
+    bot.add_cog(Events(bot))
+    
