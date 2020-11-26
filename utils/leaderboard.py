9f8049e8111008689c049984dc24765ad3260c80
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2020-11-27 05:35:01 +0700

    hello

--- utils/leaderboard.py
+++ utils/leaderboard.py
@@ -19,38 +19,33 @@ class Leaderboard(commands.Cog):
 	)
 	async def leaderboard(self, ctx, *args):
 		guild, author = ctx.guild, ctx.author
-		try:
-			if len(args) == 0:
-				guild_scores = list(database.retrieve_guild_scores(guild.id))
-				own_score = database.retrieve_score(guild.id, author.id)
-				hms_score = converter.secs_to_hms(own_score)
-				rank = guild_scores.index((own_score,)) + 1
-				leaderboard_embed = discord.Embed(title=num2words(rank, to="ordinal_num") + f" Place: {author.name}#{author.discriminator}", timestamp=datetime.now(timezone.utc), color=discord.Colour(0x100000))
+		if len(args) == 0:
+			guild_scores = list(database.retrieve_guild_scores(guild.id))
+			own_score = database.retrieve_score(guild.id, author.id)
+			hms_score = converter.secs_to_hms(own_score)
+			rank = guild_scores.index((own_score,)) + 1
+			leaderboard_embed = discord.Embed(title=num2words(rank, to="ordinal_num") + f" Place: {author.name}#{author.discriminator}", timestamp=datetime.now(timezone.utc), color=discord.Colour(0x100000))
+			leaderboard_embed.add_field(
+				name=":stopwatch: Score",
+				value=f"{hms_score[0]:02}:{hms_score[1]:02}:{hms_score[2]:02}"
+			)
+		elif len(args) == 1:
+			top10 = list(database.retrieve_top_ten(guild.id))
+			upto, i = int(args[0]), 0
+			leaderboard_embed = discord.Embed(title=f"Server rank for {guild.name}", timestamp=datetime.now(timezone.utc), color=discord.Colour(0x100000))
+			while i < upto:
+				hms_score = converter.secs_to_hms(top10[i][1])
 				leaderboard_embed.add_field(
-					name=":stopwatch: Score",
-					value=f"{hms_score[0]:02}:{hms_score[1]:02}:{hms_score[2]:02}"
+					name=num2words(i+1, to="ordinal_num") + " Place:",
+					value=guild.get_member(top10[i][0]).name + "#" + str(guild.get_member(top10[i][0]).discriminator) + ":" + f" {hms_score[0]:02}:{hms_score[1]:02}:{hms_score[2]:02}",
+					inline=False
 				)
-			elif len(args) == 1:
-				top10 = list(database.retrieve_top_ten(guild.id))
-				upto, i = int(args[0]), 0
-				leaderboard_embed = discord.Embed(title=f"Server rank for {guild.name}", timestamp=datetime.now(timezone.utc), color=discord.Colour(0x100000))
-				while i < upto:
-					hms_score = converter.secs_to_hms(top10[i][1])
-					leaderboard_embed.add_field(
-						name=num2words(i+1, to="ordinal_num") + " Place:",
-						value=guild.get_member(top10[i][0]).name + "#" + str(guild.get_member(top10[i][0]).discriminator) + ":" + f" {hms_score[0]:02}:{hms_score[1]:02}:{hms_score[2]:02}",
-						inline=False
-					)
-					i += 1
-			await ctx.send(embed=leaderboard_embed)
-		except e:
-			await ctx.send("oopsy woopsy")
-			await ctx.send(e)
+				i += 1
+		await ctx.send(embed=leaderboard_embed)
 
 	@leaderboard.error
 	async def update_error(self, ctx, error):
-		if isinstance(error, commands.BadArgument):
-			await ctx.send("Incorrect argument!")
+		await ctx.send(error)
 
 def setup(bot):
 	bot.add_cog(Leaderboard(bot))
