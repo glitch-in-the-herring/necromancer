78dcf839bf218800722b4018f8e0d86e3ea53a1b
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2020-12-07 14:05:51 +0700

    testing testing

--- utils/leaderboard.py
+++ utils/leaderboard.py
@@ -15,12 +15,41 @@ class Leaderboard(commands.Cog):
 	@commands.Cog.listener()
 	async def on_reaction_add(self, reaction, user):
 		message = reaction.message
-		if message.author == self.bot.user:	
+		if message.author == self.bot.user and user != self.bot.user:	
 			guild = message.guild
-			leaderboard_embed = message.embeds[0]
-			if leaderboard_embed.title == f"Server rank for {guild.name}":
-				page = leaderboard_embed.footer.text.split()[1]
-				print(page)
+			old_embed = message.embeds[0]
+			if old_embed.title == f"Server rank for {guild.name}":
+				old_page = int(old_embed.footer.text.split()[1])
+				server_board = list(enumerate(database.retrieve_server_board(guild.id)))
+				pages = math.floor(len(server_board)/5)
+				new_embed = discord.Embed(title=f"Server rank for {guild.name}", timestamp=datetime.now(timezone.utc), color=discord.Colour(0x100000))
+				new_embed.set_thumbnail(url=str(guild.icon_url))			
+				if reaction == "⬅️" and old_page > 1:
+					page = old_page - 1
+					await reaction.remove(user)
+				elif reaction == "➡️" and old_page < pages
+					page = old_page + 1
+					await reaction.remove(user)
+				else:
+					return
+				new_embed.set_footer(text=f"Page {page} of {pages}")
+				for y, x in server_board[(5 * (page-1)):(5 * (page))]:
+					nth_member = guild.get_member(x[0])
+					hms_score = converter.secs_to_hms(x[1])
+					if nth_member is not None:
+						new_embed.add_field(
+							name=num2words(y+1, to="ordinal_num") + " Place:",
+							value=nth_member.name + "#" + str(nth_member.discriminator) + ":" + f" {hms_score[0]:02}:{hms_score[1]:02}:{hms_score[2]:02}",
+							inline=False	
+						)
+					else:
+						new_embed.add_field(
+							name=num2words(i+1, to="ordinal_num") + " Place:",
+							value="User not found" + ":" + f" {hms_score[0]:02}:{hms_score[1]:02}:{hms_score[2]:02}",
+							inline=False	
+						)
+				await message.edit(embed=new_embed)
+
 
 	# Commands
 	# Retrieves the leaderboard
