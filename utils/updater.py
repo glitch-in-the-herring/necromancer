0b8680f1e22fc7803218a6cab029b44580263367
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2021-01-01 14:11:40 +0700

    weet

--- utils/updater.py
+++ utils/updater.py
@@ -18,7 +18,8 @@ class Updater(commands.Cog):
 		async def predicate(ctx):
 			member = ctx.author
 			permission = discord.Permissions()
-		return member.id in [int(config["sysadmin_id"])] or member.guild_permissions == permission.manage_channels
+			return member.id in [int(config["sysadmin_id"])] or member.guild_permissions == permission.manage_channels
+		return commands.check(predicate)
 
 
 	# Listeners
