78dcae8cdd51f4014d6bb701d06b524fa82d641d
glitch-in-the-herring <70984234+glitch-in-the-herring@users.noreply.github.com>
2021-02-10 08:49:00 +0700

    self ad

---
+++ utils/about.py
@@ -0,0 +1,19 @@
+import discord
+from discord.ext import commands
+
+class About(commands.Cog):
+	def __init__ (self, bot):
+		self.bot = bot
+
+	@commands.command(
+		name="about"
+	)
+	async def about(self, ctx):
+		embed = discord.Embed(
+			title="About me", 
+			colour=discord.Colour(0xdad7a0), 
+			description="Developed by red herring#5078. [check out the source code](https://github.com/glitch-in-the-herring/necromancer)"
+		)
+
+def setup(bot):
+	bot.add_cog(About(bot))
